0b36e57fb4fe6ef0bdbdba26577f4a477a40e065
Eduardo Gonzalez <eglez@ast.cam.ac.uk>
2019-08-09 01:24:22 +0100

    Bump version: 0.1.5 → 0.1.6

--- setup.py
+++ setup.py
@@ -47,6 +47,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://gitlab.ast.cam.ac.uk/imaxt/imc_pipeline',
-    version='0.1.5',
+    version='0.1.6',
     zip_safe=False,
 )
