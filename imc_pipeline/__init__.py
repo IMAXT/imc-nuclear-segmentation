3e598df7d3b8be593fac0c031e85edff4baf1eed
Eduardo Gonzalez Solares <eglez@ast.cam.ac.uk>
2021-02-18 16:11:12 +0000

    Single to double quotes

--- imc_pipeline/__init__.py
+++ imc_pipeline/__init__.py
@@ -1,6 +1,6 @@
 from .main import main  # noqa: F401
 from .schema import schema  # noqa: F401
 
-__author__ = 'Ali Dariush'
-__email__ = 'adariush@ast.cam.ac.uk'
-__version__ = '__version__ = '0.11.1''
+__author__ = "Ali Dariush"
+__email__ = "adariush@ast.cam.ac.uk"
+__version__ = "0.11.1"
--- setup.py
+++ setup.py
@@ -1,52 +1,52 @@
 from setuptools import find_packages, setup
 
-with open('README.rst') as readme_file:
+with open("README.rst") as readme_file:
     readme = readme_file.read()
 
-with open('HISTORY.rst') as history_file:
+with open("HISTORY.rst") as history_file:
     history = history_file.read()
 
 requirements = [
-    'dask',
-    'distributed',
-    'voluptuous',
-    'opencv-python==4.2.*',
-    'numpy',
-    'scipy',
-    'scikit-image',
-    'astropy',
-    'pillow',
+    "dask",
+    "distributed",
+    "voluptuous",
+    "opencv-python==4.2.*",
+    "numpy",
+    "scipy",
+    "scikit-image",
+    "astropy",
+    "pillow",
 ]
 
-setup_requirements = ['pytest-runner', 'flake8']
+setup_requirements = ["pytest-runner", "flake8"]
 
-test_requirements = ['coverage', 'pytest', 'pytest-cov', 'pytest-mock']
+test_requirements = ["coverage", "pytest", "pytest-cov", "pytest-mock"]
 
 setup(
-    author='Ali Dariush',
-    author_email='adariush@ast.cam.ac.uk',
+    author="Ali Dariush",
+    author_email="adariush@ast.cam.ac.uk",
     classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
-        'Natural Language :: English',
-        'Programming Language :: Python :: 3.7',
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3.7",
     ],
-    description='IMC pipeline.',
-    entry_points={'owl.pipelines': 'imc = imc_pipeline'},
+    description="IMC pipeline.",
+    entry_points={"owl.pipelines": "imc = imc_pipeline"},
     install_requires=requirements,
-    license='GNU General Public License v3',
-    long_description=readme + '\n\n' + history,
+    license="GNU General Public License v3",
+    long_description=readme + "\n\n" + history,
     include_package_data=True,
-    data_files=[('conf', ['conf/imc_pipeline.yaml'])],
-    keywords='imaxt, imc, owl',
-    name='imc-pipeline',
-    packages=find_packages(include=['imc_pipeline']),
+    data_files=[("conf", ["conf/imc_pipeline.yaml"])],
+    keywords="imaxt, imc, owl",
+    name="imc-pipeline",
+    packages=find_packages(include=["imc_pipeline"]),
     setup_requires=setup_requirements,
-    test_suite='tests',
+    test_suite="tests",
     tests_require=test_requirements,
-    url='https://gitlab.ast.cam.ac.uk/imaxt/imc_pipeline',
-    version='version='0.11.1'',
+    url="https://gitlab.ast.cam.ac.uk/imaxt/imc_pipeline",
+    version="0.11.1",
     zip_safe=False,
-    dependency_links=['https://imaxt.ast.cam.ac.uk/pip/imaxt-image'],
+    dependency_links=["https://imaxt.ast.cam.ac.uk/pip/imaxt-image"],
 )
